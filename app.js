1f1780e8037b6aea5855632118ea859083e16051
Tanmay Nalawade <tanmaynalawade1@gmail.com>
2026-04-26 01:17:47 -0700

    add sign-in page

--- app.js
+++ app.js
@@ -1,14 +1,32 @@
 const express = require("express");
 const path = require("path");
+const ejsMate = require("ejs-mate");
+const methodOverride = require("method-override");
 
 const app = express();
 
 require("./db"); // To run mongoose.connect() code from db.js
 
+app.engine("ejs", ejsMate);
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+  res.render("index", { title: "Home" });
+});
+
+app.get("/sign-in", (req, res) => {
+  res.render("sign-in", {
+    title: "Sign in",
+    styles: ["/sign-in.css"],
+    bodyClass: "page--full",
+    mainClass: "page page--full",
+  });
 });
 
 const port = Number.parseInt(process.env.PORT || "8080", 10);
